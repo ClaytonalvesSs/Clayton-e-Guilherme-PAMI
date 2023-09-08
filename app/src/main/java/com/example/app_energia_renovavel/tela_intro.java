0f096578056a0d32d406808e852d333ba8e0ea04
evelynkarina44 <evelyn.goncalves12@etec.sp.gov.br>
2023-09-08 20:38:03 -0300

    fazendo a tela splash e e tela de login

--- app/src/main/java/com/example/app_energia_renovavel/tela_intro.java
+++ app/src/main/java/com/example/app_energia_renovavel/tela_intro.java
@@ -2,7 +2,9 @@ package com.example.app_energia_renovavel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class tela_intro extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class tela_intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_intro);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(tela_intro.this, tela_login.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
     }
 }
--- app/src/main/java/com/example/app_energia_renovavel/tela_login.java
+++ app/src/main/java/com/example/app_energia_renovavel/tela_login.java
@@ -2,7 +2,9 @@ package com.example.app_energia_renovavel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class tela_login extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class tela_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+    }
+    public void enviar(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
